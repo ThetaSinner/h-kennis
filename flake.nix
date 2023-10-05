f06b0235ccc32cbd5b588e14e867bedcfc3a8938
ThetaSinner <ThetaSinner@noreply.github.com>
2023-10-05 02:21:39 +0100

    Scaffold

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Holochain app development";
+
+  inputs = {
+    nixpkgs.follows = "holochain/nixpkgs";
+
+    versions.url = "github:holochain/holochain?dir=versions/0_2";
+
+    versions.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.2";
+    versions.inputs.lair.url = "github:holochain/lair/lair_keystore-v0.3.0";
+
+    holochain = {
+      url = "github:holochain/holochain";
+      inputs.versions.follows = "versions";
+    };
+  };
+
+  outputs = inputs @ { ... }:
+    inputs.holochain.inputs.flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      {
+        systems = builtins.attrNames inputs.holochain.devShells;
+        perSystem =
+          { config
+          , pkgs
+          , system
+          , ...
+          }: {
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ inputs.holochain.devShells.${system}.holonix ];
+              packages = [ pkgs.nodejs-18_x ];
+            };
+          };
+      };
+}
+
